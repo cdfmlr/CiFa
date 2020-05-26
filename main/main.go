8df64d626e465eaba8edf5cd478d806ca38c3c8b
cdfmlr <cdfmlr@163.com>
2020-05-26 11:41:56 +0800

    fix(main): fix bugs causing cli params not work

--- main/main.go
+++ main/main.go
@@ -19,26 +19,26 @@ func main() {
 	// 命令行参数
 	flag.Usage = usage
 
-	confFile := *flag.String("c", "", "set the configuration `file`")
-	port := *flag.Uint("p", 9001, "set the `port` for service")
-	staticDir := *flag.String("s", "./static", "set the `static files path`")
-	tempDirPrefix := *flag.String("t", "temp.cifa.", "set the `temp dir prefix`")
+	confFile := flag.String("c", "", "set the configuration `file`")
+	port := flag.Uint("p", 9001, "set the `port` for service")
+	staticDir := flag.String("s", "./static", "set the `static_dir_path`")
+	tempDirPrefix := flag.String("t", "temp.cifa.", "set the `temp_dir_prefix`")
 
 	flag.Parse()
 
 	// 配置 app，配置文件优先级高于命令行参数
 	cifa := app.GetInstance()
 
-	if staticDir != "" {
-		cifa.Conf.StaticDir = staticDir
+	if *staticDir != "" {
+		cifa.Conf.StaticDir = *staticDir
 	}
 
-	if tempDirPrefix != "" {
-		cifa.Conf.TempDirPrefix = tempDirPrefix
+	if *tempDirPrefix != "" {
+		cifa.Conf.TempDirPrefix = *tempDirPrefix
 	}
 
-	if confFile != "" {
-		if err := util.LoadJsonFile(confFile, &cifa.Conf); err != nil {
+	if *confFile != "" {
+		if err := util.LoadJsonFile(*confFile, &cifa.Conf); err != nil {
 			logging.Error("Failed to load configuration file:", err)
 		}
 	}
@@ -51,9 +51,9 @@ func main() {
 	}
 
 	// 启动 app，监听服务
-	_, _ = fmt.Fprintf(os.Stderr, "CiFa running on localhost:%v.\n", port)
+	_, _ = fmt.Fprintf(os.Stderr, "CiFa running on localhost:%v.\n static file serve: %v\n", *port, *staticDir)
 	cifa.Run()
-	err := http.ListenAndServe(fmt.Sprintf(":%v", port), cifa.Runtime.Service)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", *port), cifa.Runtime.Service)
 
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, "http.ListenAndServe error:", err)
